39b0e07db640ff9b5a610cd19749ec15658eb158
danielrussell-brain <daniel.rb@hotmail.co.uk>
2021-11-28 11:30:28 +0000

    Added alembic migrations and sleep post view

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,9 @@
 from .database import Base
-from sqlalchemy import Column, Integer, Text, String, Boolean, ForeignKey, Float
+from sqlalchemy import Column, Integer, Text, String, Boolean, ForeignKey, Float, Interval
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-from datetime import datetime
+from datetime import datetime, timezone
 from .utils import pwd_context
 
 
@@ -16,6 +16,7 @@ class Baby(Base):
     is_awake = Column(Boolean, nullable=False, server_default="True")
     is_feeding = Column(Boolean, nullable=False, server_default="False")
     sleep_sessions = relationship("SleepSession", back_populates="baby")
+    feed_sessions = relationship("FeedSession", back_populates="baby")
 
 
 class Sleep(Base):
@@ -35,15 +36,16 @@ class SleepSession(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     sleep_start = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
     sleep_end = Column(TIMESTAMP(timezone=True), nullable=True)
-    sleep_length = Column(Float, nullable=True)
+    sleep_length = Column(Interval, nullable=True)
     baby_id = Column(Integer, ForeignKey('babies.id', ondelete="CASCADE"))
     baby = relationship("Baby", back_populates="sleep_sessions")
 
-    def set_sleep_length(self):
-        self.sleep_length = self.sleep_end - self.sleep_start
-
     def set_sleep_end(self):
-        self.sleep_end = datetime.utcnow()
+        self.sleep_end = datetime.now(timezone.utc)
+
+    def set_sleep_length(self):
+        self.set_sleep_end()
+        self.sleep_length = self.sleep_end - self.sleep_start
 
 
 class Feed(Base):
@@ -55,3 +57,21 @@ class Feed(Base):
     is_start = Column(Boolean, nullable=False)
     baby_id = Column(Integer, ForeignKey("babies.id", ondelete="CASCADE"), nullable=False)
     baby = relationship("Baby", back_populates='feeds')
+
+
+class FeedSession(Base):
+    __tablename__ = 'feedsessions'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    feed_start = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    feed_end = Column(TIMESTAMP(timezone=True), nullable=True)
+    feed_length = Column(Interval, nullable=True)
+    baby_id = Column(Integer, ForeignKey('babies.id', ondelete="CASCADE"))
+    baby = relationship("Baby", back_populates="feed_sessions")
+
+    def set_feed_end(self):
+        self.feed_end = datetime.now(timezone.utc)
+
+    def set_feed_length(self):
+        self.set_feed_end()
+        self.feed_length = self.feed_end - self.feed_start
--- backend/routes/sleep.py
+++ backend/routes/sleep.py
@@ -1,4 +1,11 @@
 from fastapi import Depends, status, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+from ..database import get_db
+from .. import models
+
+# This needs to be changed to allow multiple babies
+# Currently assuming only one child
+BABY_CONSTANT = 1
 
 
 router = APIRouter(
@@ -9,3 +16,49 @@ router = APIRouter(
 @router.get("/")
 def sleep_get():
     return {"message": "Sleeps!"}
+
+
+@router.post("/", status_code=status.HTTP_200_OK)
+def sleep_post(db: Session = Depends(get_db)):
+    # get the baby
+    baby_query = db.query(models.Baby).filter(models.Baby.id == BABY_CONSTANT)
+    baby = baby_query.first()
+    # if the baby is awake
+    if baby.is_awake:
+        # create a new sleep session
+        sleep_session = models.SleepSession(
+            baby_id=BABY_CONSTANT
+        )
+        # log the sleep in the sleep model
+        sleep = models.Sleep(
+            baby_id=BABY_CONSTANT,
+            is_awake=False,
+            sleep_id=1
+        )
+        # set the baby to asleep
+        baby.is_awake = False
+        # commit everything
+        db.add(sleep_session)
+        db.add(sleep)
+        db.commit()
+        return {"message": "Baby is now asleep!"}
+    else:
+        # get the most recent sleep session
+        sleep_session = db.query(models.SleepSession)\
+            .filter(models.SleepSession.baby_id==BABY_CONSTANT)\
+            .order_by(models.SleepSession.sleep_start.desc())\
+            .first()
+        # set the timestamp for baby waking
+        sleep_session.set_sleep_length()
+        # set a timestamp in the second baby sleep model
+        sleep = models.Sleep(
+            baby_id=BABY_CONSTANT,
+            is_awake=True,
+            sleep_id=1
+        )
+        # set the baby to awake
+        baby.is_awake = True
+        db.add(sleep)
+        db.commit()
+        return {"message": "Baby is now awake!"}
+
